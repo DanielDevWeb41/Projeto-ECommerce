314a3b39af0747bf71efeef1bff555b30ae2c5c5
daniel <danielk15656@gmail.com>
2025-03-18 11:44:53 -0300

    teste_update2.5_main

--- js/script.js
+++ js/script.js
@@ -4,3 +4,69 @@ let nav = document.getElementById("nv")
 bar.onclick = () => {
     nav.classList.toggle("active")
 }
+
+
+//background Switch
+
+let left = document.getElementById("left")
+let right = document.getElementById("right")
+let content = document.querySelector(".content")
+
+right.onclick = function () {
+    content.style.backgroundImage = "url(../img/hero-2.jpg)"
+    left.style.color = "#e53637";
+    right.style.color = "#000000";
+}
+
+left.onclick = function () {
+    content.style.backgroundImage = "url(../img/hero-1.jpg)"
+    right.style.color = "#e53637";
+    left.style.color = "#000000";
+}
+
+// product switch
+
+let links = document.querySelectorAll(".links h1")
+let arrivals = document.querySelectorAll(".arrivals")
+let hot = document.querySelectorAll(".hot")
+
+links[1].onclick = function () {
+    links[1].classList.add('act')
+    links[0].classList.remove('act')
+    links[3].classList.remove('act')
+
+    hot.forEach((e) => {
+        e.style.display = "none"
+    })
+    arrivals.forEach((e) => {
+        e.style.display = "block"
+    })
+}
+
+links[2].onclick = function () {
+    links[2].classList.add('act')
+    links[0].classList.remove('act')
+    links[1].classList.remove('act')
+
+    hot.forEach((e) => {
+        e.style.display = "block"
+    })
+    arrivals.forEach((e) => {
+        e.style.display = "none"
+    })
+}
+
+links[0].onclick = function () {
+    links[2].classList.remove('act')
+    links[0].classList.add('act')
+    links[1].classList.remove('act')
+
+    hot.forEach((e) => {
+        e.style.display = "block"
+    })
+    arrivals.forEach((e) => {
+        e.style.display = "block"
+    })
+}
+
+
